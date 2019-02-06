5e39ef2107a5b3929c7570eb2fa18560a201f3e8
Rina Schiller <schiller.rina@gmail.com>
2019-02-05 22:38:12 -0500

    fixing template, adding facebook sdk and mapbox api

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
-from forms import *
 import os
 
 #----------------------------------------------------------------------------#
@@ -49,26 +48,6 @@ def home():
 @app.route('/about')
 def about():
     return render_template('pages/placeholder.about.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
-
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
-
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
-# Error handlers.
 
 
 @app.errorhandler(500)
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,38 @@
-(function() {
+$( document ).ready(function() {
 
-}).call(this);
+    console.log( "ready!" );
+    var event_ids = []
+
+    $(document).on(
+        'fbload',  //  <---- HERE'S OUR CUSTOM EVENT BEING LISTENED FOR
+        function(){
+        FB.api(
+          '/1886000451690456/events',
+          'GET',
+          {},
+          function(response) {
+              // Insert your code here
+              console.log(response);
+          }
+        );
+
+        FB.ui({
+            method: 'share_open_graph',
+            action_type: 'og.likes',
+            action_properties: JSON.stringify({
+              object:'https://developers.facebook.com/docs/javascript/examples',
+            })
+          }, function(response){
+            // Debug response (optional)
+            console.log(response);
+          });
+              
+    });
+        
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoicmluYXNjaGlsbGVyIiwiYSI6ImNqcm1sZmNkcjBra24zeW1uMHQycXY3a2oifQ.hI2dy3oRA5qb48kXdMn5ug';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11'
+    });
+});
