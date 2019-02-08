33daa8afc33587c5ebdb376f98161eb4e5be1e73
Rina Schiller <schiller.rina@gmail.com>
2019-02-07 23:10:45 -0500

    got facebook event data populating on mapbox

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,49 @@
 $( document ).ready(function() {
-
-    console.log( "ready!" );
-    var event_ids = []
-
-    $(document).on( 'fbload',  // Called when Facebook SDK initializes
-        function(){
-        var token = "EAADZAHQ1ZBg74BAJEdmpIw70gnxvYtMQynnLiMCjV9v57cXBjcSk5HMTAhgF5vZAVJvRgw0ojDDCUlRJQLFUVIzZBOjQHIUBxVviGmDxHZC2f9Emb6wXBcFeN9uveLbyQMGrFZApCBPOUYglXkhf5i9wozGhxvDmVUJZCbPrUWZAiF8HPBuKyenXft8kBepPNnJ3zrm9GjfaDgZDZD"
-        
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-              var accessToken = response.authResponse.accessToken;
-              FB.api(
-                '/1886000451690456/events',
-                'GET',
-                {access_token: accessToken},
-                function(response) {
-                    console.log(response);
-                }
-              );
-            
-            } 
-          } );
-
-        
-
-    
-              
-    });
-        
-
     mapboxgl.accessToken = 'pk.eyJ1IjoicmluYXNjaGlsbGVyIiwiYSI6ImNqcm1sZmNkcjBra24zeW1uMHQycXY3a2oifQ.hI2dy3oRA5qb48kXdMn5ug';
     var map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
+        center: [-88.8422, 41.3471],
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 5
+    });
+    
+    var ll = new mapboxgl.LngLat(-73.9749, 40.7736);
+    $(document).on( 'fbload',  // Called when Facebook SDK initializes
+        function(){ FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                var accessToken = response.authResponse.accessToken;
+                FB.api('/1886000451690456/events','GET', {access_token: accessToken},
+                    function(response) {
+                        console.log(response.data);
+                        for (var event of response.data){
+                            var lat = _.get(event, 'place.location.latitude');
+                            var long = _.get(event, 'place.location.longitude');
+                            if (lat != undefined && long != undefined){
+                                var popup = new mapboxgl.Popup({ offset: 25 })
+                                    .setHTML(getPopupHTML(event));
+                                console.log(lat + ", " + long);
+                                new mapboxgl.Marker()
+                                .setLngLat([long, lat])
+                                .setPopup(popup)
+                                .addTo(map);
+
+                                
+
+                            }
+                           
+                        }
+                        
+                    }
+                );
+            
+                } 
+            });              
     });
 });
+
+function getPopupHTML(event){
+    var header = "<b>" + event.name + "</b>"
+    var description = "<p>" + event.description + "</p>"
+    var startTime = "<p>" + event.start_time+ "</p>"
+    return header + "</n>" + description + "</n>" + startTime
+}
